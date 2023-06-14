58e65ffa4fcaf38e4b5e5e9db61bb270b59532a3
Zilya Nuretdinova <hfast@student.21-school.ru>
2023-06-14 17:35:09 +0300

    fill db with python

---
+++ module_13_db2/add_from_csv_to_db.py
@@ -0,0 +1,19 @@
+import sqlite3
+
+def add_from_csv_to_db(cur, filename):
+    records = []
+    with open(filename, 'r') as f:
+        lines = f.readlines()
+        for line in lines[1:]:
+            isbn, book_name, author, publish_year = line.split(',')
+            records.append(tuple([book_name, author, int(publish_year), isbn]))
+
+        for record in records:
+            cur.execute("""INSERT INTO `table_books` (book_name, author, publish_year, isbn) VALUES (?,?,?,?)""", record)
+
+
+if __name__ == "__main__":
+    with sqlite3.connect('practise.db') as conn:
+        cur = conn.cursor()
+        add_from_csv_to_db(cur, 'book_list.csv')
+        cur.connection.commit()
